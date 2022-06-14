6ffaee91a6129a158e308dbd0d04212d4cef0434
Berke Yay <105660365+Berke-Yay@users.noreply.github.com>
2022-06-14 16:43:05 +0300

    Update ones_and_zeros.java

--- ones_and_zeros.java
+++ ones_and_zeros.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class ones_and_zeros {
     public static int ConvertBinaryArrayToInt(List<Integer> binary) {
-        // Your Code
+        // This function will return the integer value of a binary number which is given as a list.
         int result=0;
         int length = binary.size();
         for (int i=0;i<binary.size();i++) {
