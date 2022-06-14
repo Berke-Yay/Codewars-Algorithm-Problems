f7302c2dc3d50199c2e5943ab9426a679774e1fa
Berke Yay <105660365+Berke-Yay@users.noreply.github.com>
2022-06-14 16:47:30 +0300

    Update printer_error.java

--- printer_error.java
+++ printer_error.java
@@ -2,7 +2,9 @@ package codewars;
 
 public class printer_error {
     public static String printerError(String s) {
-        // your code
+        // In a print office, a printer needs to take a dye as an input. The dyes are represented with letters.
+        //And there only dyes from 'a' to 'm'. The others are considered as errors.
+        //We need to find the ratio of errors and total letters that are given.
         int error_counter=0;
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
         for(int i=0;i<s.length();i++){
@@ -14,6 +16,7 @@ public class printer_error {
         return error_counter+"/"+s.length();
     }
     //A better solution
+    //The code below is the solution from Codewars' public solutions part. I just wrote this to remind this way. In other words, the below part is not my code.
     public static String printerError2(String s) {
         return s.replaceAll("[a-m]", "").length() + "/" + s.length();
     }
